f3cbb27d5a2e10b00691595d44063efd9ee6e45b
bruno matheus <bruno@boxgroup.net.br>
2021-08-27 20:16:26 -0300

    primeiro commit projeto

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <Title>Explere repositórios no Github</Title>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Relatory: React.FC = () => {
+  return (
+    <>
+      <h1>Repository</h1>
+    </>
+  );
+};
+
+export default Relatory;
