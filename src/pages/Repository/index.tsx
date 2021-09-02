8981c3c6960ae4bfaed456fe7c9d5f046de8667e
bruno matheus <bruno@boxgroup.net.br>
2021-09-02 12:14:20 -0300

    desenvolvimento da tela de pesquisa e inicio da tela do repositorio

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,11 +1,66 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-const Relatory: React.FC = () => {
+import logoImg from '../../assets/logo.svg'
+import { Header, RepositoryInfo, Issues} from './styles';  
+
+
+interface RepositoryParams {
+  repository: string
+} 
+
+const Repository: React.FC = () => {
+  const { params } = useRouteMatch<RepositoryParams>();
   return (
     <>
-      <h1>Repository</h1>
+    <Header>
+      <img src={logoImg } alt="Github Explorer" />
+      <Link to="/">
+      <FiChevronLeft size={16} />
+      voltar
+      </Link>
+    </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars.githubusercontent.com/u/72323650?s=60&v=4" alt="ROCE"/>
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descricao do repostitorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>          
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+
+          <Link
+              to={"asdasdasd"}>
+
+            <div>
+              <strong>asdasd</strong>
+              <p>asdasd</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </Link>        
+      </Issues>
     </>
   );
 };
 
-export default Relatory;
+export default Repository;
